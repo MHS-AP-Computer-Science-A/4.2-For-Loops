3d13f72345365177734f3e0590c7dcb17ac76ef1
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-06-03 11:03:28 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// For loops define a loop variable,
+		// provide a looping condition based on that variable,
+		// and define how to iterate the variable
+		
+		//Count from 0 to 9 by 1s
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+		
+		//Count from 100 to 0 by 2s
+		for (int i = 100; i >=0; i-=2) {
+			System.out.println(i);
+		}
+	}
+
+}
